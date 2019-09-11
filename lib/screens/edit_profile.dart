cdca86dd926373510af04169d3f372e77f2d2182
SangYeon <sam@superants.io>
2019-09-11 16:33:40 +0900

    initial profile editing screen

---
+++ lib/screens/edit_profile.dart
@@ -0,0 +1,170 @@
+import "package:flutter/material.dart";
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'main.dart'; //for currentuser & google signin instance
+// import 'models/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class EditProfilePage extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController bioController = TextEditingController();
+
+  changeProfilePhoto(BuildContext parentContext) {
+    return showDialog(
+      context: parentContext,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Change Photo'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                    'Changing your profile photo has not been implemented yet'),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+ /*  applyChanges() {
+    Firestore.instance
+        .collection('insta_users')
+        .document(currentUserModel.id)
+        .updateData({
+      "displayName": nameController.text,
+      "bio": bioController.text,
+    });
+  } */
+
+  Widget buildTextField({String name, TextEditingController controller}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(top: 12.0),
+          child: Text(
+            name,
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            hintText: name,
+          ),
+        ),
+      ],
+    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center();}/* FutureBuilder(
+        future: Firestore.instance
+            .collection('insta_users')
+            .document(currentUserModel.id)
+            .get(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Container(
+                alignment: FractionalOffset.center,
+                child: CircularProgressIndicator());
+
+          User user = User.fromDocument(snapshot.data);
+
+          nameController.text = user.displayName;
+          bioController.text = user.bio;
+
+          return Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(currentUserModel.photoUrl),
+                  radius: 50.0,
+                ),
+              ),
+              FlatButton(
+                  onPressed: () {
+                    changeProfilePhoto(context);
+                  },
+                  child: Text(
+                    "Change Photo",
+                    style: const TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  )),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: <Widget>[
+                    buildTextField(name: "Name", controller: nameController),
+                    buildTextField(name: "Bio", controller: bioController),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: MaterialButton(
+                    onPressed: () => {_logout(context)},
+                    child: Text("Logout")
+
+                )
+              )
+            ],
+          );
+        });
+  }
+
+  void _logout(BuildContext context) async {
+    print("logout");
+    await auth.signOut();
+    await googleSignIn.signOut();
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+
+    currentUserModel = null;
+
+    Navigator.pop(context);
+  }
+}
+
+
+class User {
+  final String email;
+  final String id;
+  final String photoUrl;
+  final String username;
+  final String displayName;
+  final String bio;
+  final Map followers;
+  final Map following;
+
+  const User(
+      {this.username,
+      this.id,
+      this.photoUrl,
+      this.email,
+      this.displayName,
+      this.bio,
+      this.followers,
+      this.following});
+
+  factory User.fromDocument(DocumentSnapshot document) {
+    return User(
+      email: document['email'],
+      username: document['username'],
+      photoUrl: document['photoUrl'],
+      id: document.documentID,
+      displayName: document['displayName'],
+      bio: document['bio'],
+      followers: document['followers'],
+      following: document['following'],
+    );
+  }
+} */
