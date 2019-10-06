15f5c5864e3eb72f47b62301230077a695395d69
SangYeon <sam@superants.io>
2019-10-06 13:42:23 +0900

    -draft for translation selecting page

---
+++ lib/screens/test/page_dev_test_translation_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class TranslationListScreen extends StatefulWidget {
+  TranslationListScreen({Key key}) : super(key: key);
+
+  _TranslationListScreenState createState() => _TranslationListScreenState();
+}
+
+class _TranslationListScreenState extends State<TranslationListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       
+    );
+  }
+}
