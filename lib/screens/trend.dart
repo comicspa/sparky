e8edf4afa0f1053fbefc3f8893f9b0f694ad9856
SangYeon <sam@superants.io>
2019-10-29 17:01:31 +0900

    changing view count

--- lib/screens/trend.dart
+++ lib/screens/trend.dart
@@ -504,7 +504,7 @@ class TrendCardList extends StatelessWidget {
                             child: Align(
                               alignment: Alignment.centerLeft,
                               child: Text(
-                                snapshot.data[index].creatorId,
+                                snapshot.data[index].creatorName,
                                 textAlign: TextAlign.right,
                                 maxLines: 1,
                                 overflow: TextOverflow.ellipsis,
@@ -536,7 +536,7 @@ class TrendCardList extends StatelessWidget {
                               child: Align(
                                 alignment: Alignment.centerRight,
                                 child: Text(
-                                  '250,589,938', //Todo need to create 조회수 data
+                                  snapshot.data[index].viewCount, 
                                   maxLines: 1,
                                   overflow: TextOverflow.ellipsis,
                                   textAlign: TextAlign.right,
